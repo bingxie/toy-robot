54896b303c452e31bf67d12f1ec8910110b28bcc
Bing Xie <bing708@gmail.com>
2016-10-17 15:21:44 +1100

    Add new back command to factory

--- test/lib/commands/factory_test.rb
+++ test/lib/commands/factory_test.rb
@@ -5,6 +5,7 @@ module Commands
     def test_build
       assert_equal Commands::Place,  Commands::Factory.build('PLACE 1,2,EAST')
       assert_equal Commands::Move,   Commands::Factory.build('MOVE')
+      assert_equal Commands::Back,   Commands::Factory.build('BACK')
       assert_equal Commands::Left,   Commands::Factory.build('LEFT')
       assert_equal Commands::Right,  Commands::Factory.build('RIGHT')
       assert_equal Commands::Report, Commands::Factory.build('REPORT')
